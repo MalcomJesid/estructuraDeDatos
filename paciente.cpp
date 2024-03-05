2348d3c9e81f58fc15b5165d393a346ef08181d9
MalcomJesid <malconyfigue@gmail.com>
2024-03-05 11:32:34 -0500

    Se agrega el proyecto

---
+++ paciente.cpp
@@ -0,0 +1,8 @@
+/*Realizar un programa que permita registrar pacientes, se debe captura el nombre del paciente y el numero de documento.
+
+1. Mostrar los pacientes de tal manera que se muestre 1, 2, 3, etc según el orden de registro (Colas).
+
+2. El proyecto ser subido por cada miembro del grupo en GitHub. */
+
+
+
